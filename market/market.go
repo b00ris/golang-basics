00c335e2b9327d0376a8752e7895cd3d7642ceac
hedgfog <hedg.fog@gmail.com>
2019-12-27 02:17:46 +0400

    Market on Test System

--- market/market.go
+++ market/market.go
@@ -1,47 +1,102 @@
 package market
 
-var Users = map[string]float32{}
-var Products = map[string]float32{}
+import (
+	"fmt"
+	"sort"
+)
 
+const (
+	ASC = iota
+	DESC
+	MANY
+)
+
+type pair struct {
+	key   string
+	value float32
+}
+
+type Product struct {
+	Name  string
+	Price float32
+}
 type Order struct {
 	User     string
-	Products []string
+	Products []Product
 }
 
-func CalcOrder(order Order) float32 {
+func CalcOrder(order Order, users map[string]float32) float32 {
 	var total float32
 	for _, product := range order.Products {
-		total += Products[product]
+		total += product.Price
 	}
-	value, okay := Users[order.User]
+	value, okay := users[order.User]
 	if okay == true && value >= total {
-		Users[order.User] -= total
+		users[order.User] -= total
 	}
 	return total
 }
 
-func CalcOrderWithSaveSpeed(order Order) float32 {
+func CalcOrderWithSpeedCache(order Order, users map[string]float32, cache map[string]float32) float32 {
+
 	var total float32
-	for _, product := range order.Products {
-		total += Products[product]
-	}
-	value, okay := Users[order.User]
-	if okay == true && value >= total {
-		Users[order.User] -= total
-	}
-	return total
-}
 
-func addProduct(name string, price float32) {
-	_, okay := Products[name]
-	if okay == false {
-		Products[name] = price
+	orderKey := ""
+	for _, v := range order.Products {
+		orderKey += v.Name
 	}
-}
-
-func updateProduct(name string, price float32) {
-	_, okay := Products[name]
+	v, okay := cache[orderKey]
 	if okay == true {
-		Products[name] = price
+		total = v
+	} else {
+		for _, product := range order.Products {
+			total += product.Price
+		}
+		cache[orderKey] = total
 	}
+	value, okay := users[order.User]
+	if okay == true && value >= total {
+		users[order.User] -= total
+	} else {
+		// fmt.Println(order.User, " не имеет требуемой суммы")
+	}
+	return total
+}
+
+func PrintUsers(typeSort int, users map[string]float32) {
+	switch typeSort {
+	case ASC:
+		keys := make([]string, 0, len(users))
+		for k := range users {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, k := range keys {
+			fmt.Println(k, users[k])
+		}
+	case DESC:
+		keys := make([]string, 0, len(users))
+		for k := range users {
+			keys = append(keys, k)
+		}
+		sort.Sort(sort.Reverse(sort.StringSlice(keys)))
+		for _, k := range keys {
+			fmt.Println(k, users[k])
+		}
+	case MANY:
+		values := make([]pair, len(users))
+		i := 0
+		for k, v := range users {
+			values[i] = pair{k, v}
+			i++
+		}
+		sort.Slice(values, func(i, j int) bool {
+			return values[i].value > values[j].value
+		})
+
+		for _, v := range values {
+			fmt.Println(v.key, v.value)
+		}
+	}
+
 }
--- market/tests/market_test.go
+++ market/tests/market_test.go
@@ -2,29 +2,101 @@ package tests
 
 import (
 	"lessons/basics/market"
+	"math/rand"
 	"testing"
 )
 
+const (
+	ASC = iota
+	DESC
+	MANY
+)
+
 func TestCalcOrder(t *testing.T) {
-	Initialize()
+	products := InitProducts()
+	users := InitUsers()
+	orders := InitOrder(products, users)
 
-	var order = market.Order{
-		User: "Gates", Products: []string{"Whisky"},
-	}
+	market.CalcOrder(orders[1], users)
 
-	var price = market.CalcOrder(order)
-	t.Log(price)
-	t.Log(market.Users["Gates"])
+	market.PrintUsers(DESC, users)
+
+	//t.Log(price)
+	//t.Log(users["Gates"])
 }
 
-func Initialize() {
-	market.Users["Kevin"] = 500
-	market.Users["Gates"] = 1_000_000_000
-	market.Users["Ford"] = 500_000_000
+func BenchmarkCalcOrder(b *testing.B) {
+	products := InitProducts()
+	users := InitUsers()
+	orders := InitOrder(products, users)
+	b.ResetTimer()
 
-	market.Products["Pineapple"] = 15.0
-	market.Products["Discount"] = 50.0
-	market.Products["Whisky"] = 5_000.0
-	market.Products["Meat"] = 125.0
-	market.Products["Chocolate"] = 35.5
+	for i := 0; i < b.N; i++ {
+		market.CalcOrder(orders[rand.Intn(100)], users)
+	}
+}
+
+func BenchmarkCalcOrderSpeedCache(b *testing.B) {
+	products := InitProducts()
+	users := InitUsers()
+	orders := InitOrder(products, users)
+
+	cache := map[string]float32{}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		market.CalcOrderWithSpeedCache(orders[rand.Intn(100)], users, cache)
+	}
+}
+
+func InitUsers() map[string]float32 {
+	return map[string]float32{
+		"Kevin": 1_500_000_000,
+		"Gates": 1_000_000_000,
+		"Ford":  500_000_000,
+	}
+}
+
+func InitProducts() []market.Product {
+	return []market.Product{
+		{"Pineapple", 15.0},
+		{"Discount", 50.0},
+		{"Meat", 125.0},
+		{"Whisky", 5_000.0},
+		{"Pineapple", 15.0},
+		{"Chocolate", 35.5},
+		{"Banana", 25.25},
+		{"Mango", 10.0},
+		{"Marakua", 123.0},
+		{"Potato", 2.0},
+		{"Tea", 10.0},
+		{"Coffee", 100.0},
+	}
+}
+func InitOrder(products []market.Product, users map[string]float32) []market.Order {
+	keyUsers := make([]string, 0, len(users))
+	for k := range users {
+		keyUsers = append(keyUsers, k)
+	}
+
+	orders := make([]market.Order, 100)
+	for i := 0; i < 100; i++ {
+		orders[i] = getOrder(keyUsers, products)
+	}
+
+	return orders
+}
+
+func getOrder(userList []string, productList []market.Product) market.Order {
+	length := rand.Intn(100)
+	products := make([]market.Product, length)
+
+	for i := 0; i < length; i++ {
+		products[i] = productList[rand.Intn(len(productList))]
+	}
+
+	return market.Order{
+		User:     userList[rand.Intn(len(userList))],
+		Products: products,
+	}
 }
